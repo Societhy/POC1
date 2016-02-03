46dcfc2cd2da5ff18e9755aabe66546bce022ccd
voinne_c <cedric.voinnet@epitech.eu>
2016-02-03 18:25:08 +0100

    User can join an orga MUST IMPLEMENT VERIFICATION IF USER IS ALREADY IN ORGA

--- truffle_Societhapp/server/mongoTest.js
+++ truffle_Societhapp/server/mongoTest.js
@@ -14,11 +14,11 @@ var finishedQuery = function (db) {
     }
 }
 
-var insertNewUser = function (db, collection, newUser) {
+var insertNewUser = function (db, newUser) {
     queriesCount++;
     console.log("Insertion of new user.");
 
-    collection.insertOne(newUser, function(err, result) {
+    db.collection('users').insertOne(newUser, function(err, result) {
         assert.equal(err, null);
         console.log("Inserted new user in users collection.");
         finishedQuery(db);
@@ -28,25 +28,24 @@ var insertNewUser = function (db, collection, newUser) {
 var existUser = function (db, newUser, callback) {
     queriesCount++;
     console.log("Searching user from db.");
-    var collection = db.collection('users');
-    var cursor = collection.find({'address':newUser.address});
+    var cursor = db.collection('users').find({'address':newUser.address});
 
     cursor.count(function(err, nb){
         assert.equal(err, null);
         if (nb != 0){
             console.log("User already exists.");
         } else {
-            callback(db, collection, newUser);
+            callback(db, newUser);
         }
         finishedQuery(db);
     });
 };
 
-var insertNewOrga = function (db, collection, newOrga) {
+var insertNewOrga = function (db, newOrga) {
     queriesCount++;
     console.log("Insertion of new orga.");
 
-    collection.insertOne(newOrga, function(err, result) {
+    db.collection('orga').insertOne(newOrga, function(err, result) {
         assert.equal(err, null);
         console.log("Inserted new orga in orga collection.");
         finishedQuery(db);
@@ -56,31 +55,49 @@ var insertNewOrga = function (db, collection, newOrga) {
 var existOrga = function (db, newOrga, callback) {
     queriesCount++;
     console.log("Searching orga from db.");
-    var collection = db.collection('orga');
-    var cursor = collection.find({'name':newOrga.name});
+    var cursor = db.collection('orga').find({'name':newOrga.name});
 
     cursor.count(function(err, nb){
         assert.equal(err, null);
         if (nb != 0) {
             console.log("Orga already exists.");
         } else {
-            callback(db, collection, newOrga);
+            callback(db, newOrga);
         }
         finishedQuery(db);
     });
 }
 
+var addOrgaToUserListOrga = function(db, newUser, newOrga) {
+    queriesCount++;
+    console.log("Adding", newOrga.name, "to list orga for", newUser.address);
+
+    db.collection('users').updateOne({'address':newUser.address}, {$push: {'listOrga': newOrga.name}}, function (err, result) {
+        assert.equal(err, null);
+        finishedQuery(db);
+    });
+};
+
+var addUserToOrgaMemberList = function(db, newOrga, newUser) {
+    queriesCount++;
+    console.log("Adding", newUser.address, "to list member for", newOrga.name);
+
+    db.collection('orga').updateOne({'name':newOrga.name}, {$push : {memberList: {'user': newUser.address, 'right': {'admin':true, 'proposeDonation':true}}}}, function (err, result) {
+        assert.equal(err, null);
+        finishedQuery(db);
+    });
+};
+
 var userJoinOrga = function (db, newUser, newOrga) {
     queriesCount++;
-    var collection = db.collection('orga');
-    var cursor = collection.find({'name':newOrga.name});
+    var cursor = db.collection('orga').find({'name':newOrga.name});
 
     cursor.count(function (err, nb) {
         assert.equal(err, null);
         if (nb != 0){
             console.log("Orga exists");
-            // addOrgaToUserListOrga();
-            // addUserToOrgaMemberList();
+            addOrgaToUserListOrga(db, newUser, newOrga);
+            addUserToOrgaMemberList(db, newOrga, newUser);
         } else {
             console.log("Orga doesn't exists");
         }
@@ -94,7 +111,7 @@ MongoClient.connect(url, function(err, db) {
     console.log("Connected to db.");
 
     var newUser = {"address":"0xa51c9ea5", "firstname":"tata", "lastname":"test", "mail":"tata@test.com", "photo":null, "listOrga":[], "transHisto":[], "infos":[]};
-    var newOrga = {"name":"Croix Rouge", "memberList":[], "transHisto":[], "actualities":[]};
+    var newOrga = {"name":"Medecins sans frontiere", "memberList":[], "transHisto":[], "actualities":[]};
 
     existUser(db, newUser, insertNewUser);
     existOrga(db, newOrga, insertNewOrga);
