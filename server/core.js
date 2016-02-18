1d0caad64b50787fe6361cc03273b248c57f28e1
Cédric Voinnet <cedric.voinnet@epitech.eu>
2016-02-18 15:56:21 +0100

    recup des photo et store dans la db

--- server/core.js
+++ server/core.js
@@ -7,15 +7,17 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var organisation = require('./routes/organisation')
-var register = require('./routes/register')
-var debug = require('./routes/debug')
+var organisation = require('./routes/organisation');
+var register = require('./routes/register');
+var debug = require('./routes/debug');
 
 var app = express();
-var db = require('./database/db')
+var db = require('./database/db');
 var dbUser = require('./database/users');
 var dbOrga = require('./database/orga');
-var url = 'mongodb://localhost:27017/test'
+var url = 'mongodb://localhost:27017/test';
+
+var fs = require('fs');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,48 +33,76 @@ app.use("/contract", express.static(path.join(__dirname, "../environments/develo
 
 db.connect(url, function (err) {
     if (err){
-        console.log('Unable to connect to Mongo.')
-        process.exit(1)
+        console.log('Unable to connect to Mongo.');
+        process.exit(1);
     } else {
         console.log('Connected to db');
 
         // USAGE: ADD NEW USER
-        var user1 = {"addresses":["0x00000001", "0x0000000a"], "firstname":"user1", "lastname":"user1", "mail":"u@1.com", "photo":null, "listOrga":[], "transHisto":[], "infos":[]}
+        var user1 = {"addresses":["0x00000001", "0x0000000a"], "firstname":"user1", "lastname":"user1", "mail":"u@1.com", "photo":null, "listOrga":[], "transHisto":[], "infos":[]};
         dbUser.addNewUser(user1, function(ret) {
             if (!ret.status)
             console.log(ret.message);
             else
             console.log('User well added');
-        })
+        });
 
         // USAGE: ADD NEW ORGA
-        var MSF = {"name":"Medecins Sans Frontiere", "memberList":[], "transHisto":[], "actualities":[]}
+        var MSF = {"name":"Medecins Sans Frontiere", "memberList":[], "transHisto":[], "actualities":[]};
         dbOrga.addNewOrga(MSF, function(ret) {
             if (!ret.status)
             console.log(ret.message);
             else
             console.log('Orga well added');
-        })
+        });
 
         // USAGE: GET USER
         dbUser.getUserByAddress("0xea662181", function(ret) {
             if (!ret.status) {
-                console.log('Error:', ret.message)
-                return
+                console.log('Error:', ret.message);
+                return;
             }
-            console.log(ret.body.firstname, ret.body.lastname)
-        })
+            console.log(ret.body.firstname, ret.body.lastname);
+        });
 
         // USAGE: GET ORGA
-        dbOrga.getOrgaByName('unicef ?', function(ret) {
+        dbOrga.getOrgaByName('unicef', function(ret) {
             if (!ret.status) {
-                console.log('Error:', ret.message)
-                return
+                console.log('Error:', ret.message);
+                return;
             }
-            console.log(ret.body.name, ret.body.memberList)
-        })
+            console.log(ret.body.name, ret.body.memberList);
+        });
+
+        // USAGE: CHANGE USER PROFILE PICTURE
+        fs.readFile("./test/img/grout_r.jpg", 'base64', function(err, data){
+            if (err) {
+                return console.log(err);
+            }
+            dbUser.changeProfilePicture({addresses:'0x00000002'}, data, function(ret) {
+                if (!ret.status){
+                    console.log(ret.message);
+                } else {
+                    console.log(ret.message);
+                }
+            });
+        });
+
+        // USAGE: GET USER PROFILE PICTURE
+        dbUser.getProfilePicture({addresses:'0x00000002'}, function(ret) {
+            if (!ret.status) {
+                console.log(ret.message);
+            } else {
+                fs.writeFile('./grout_lol.jpg', ret.body.data, 'base64', function(err){
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log('File created.');
+                });
+            }
+        });
     }
-})
+});
 
 //routing
 app.use('/', index);
