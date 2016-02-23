a6501ae2b7787b4704cc6ab55a77c9043bd3e334
Simon Vadée <simon.vadee@epitech.eu>
2016-02-23 16:02:10 +0100

    change routes

--- server/core.js
+++ server/core.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, 'public')));
-app.use("/contract", express.static(path.join(__dirname, "../environments/development/build")));
+app.use("/eth", express.static(path.join(__dirname, "../environments/development/build")));
 
 db.connect(url, function(err) {
     if (err) {
