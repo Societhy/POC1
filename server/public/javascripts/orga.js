bd19a3d704b1ff25096a4dd99bd730745ed80e58
grout_r <grout_r>
2016-02-26 17:44:02 +0100

    create prj a partir de la page orga OK

--- server/public/javascripts/orga.js
+++ server/public/javascripts/orga.js
@@ -4,8 +4,11 @@
 
 $("#createProject").on('click', function() {
     console.log($('#projectName').val());
-    return;
     var socket = io('http://localhost:3000');
+    // TODO: GERER L'ADDR
+    var addr = '0x' + (function co(lor) {
+            return (lor += [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random() * 16)]) && (lor.length == 8) ? lor : co(lor);
+        })('');
 
-    socket.emit('newProject', {name: $('#projectName').val(), desc: $('#projectDesc').val()});
+    socket.emit('newProject', {name: $('#projectName').val(), desc: $('#projectDesc').val(), addr:addr});
 });
