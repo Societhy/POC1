2c76eab35887c6d8089efd5153c67ec3d12f61ea
grout_r <grout_r>
2016-02-29 17:17:47 +0100

    req api sur pjct

--- server/api/api.js
+++ server/api/api.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var user = require('../database/user')
-var orga = require('../database/orga')
+var user = require('../database/user');
+var orga = require('../database/orga');
+var proj = require('../database/project');
 
 router.get('/', function(req, res, next)
 {
@@ -61,6 +62,21 @@ router.get('/orga/all', function(req, res, next)
 router.get('/orga/:addr', function(req, res, next)
 {
     orga.getOrga(req.params.addr, function (ret)
+    {
+        if (!ret.status)
+        {
+            var err = new Error(ret.message);
+            err.status = 404;
+            next(err);
+            return;
+        }
+        res.send(ret.object);
+    });
+});
+
+router.get('project/:addr', function(req, res, next)
+{
+    proj.getProject("0xaa33acc9", function(ret)
     {
         if (!ret.status)
         {
