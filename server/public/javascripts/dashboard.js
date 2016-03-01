30a508dea8bd8cd3832de66ea1fe8c9499b5f5c4
Titouan <titouan.theophile@gmail.com>
2016-03-01 22:37:57 +0100

    push for cedric

--- server/public/javascripts/dashboard.js
+++ server/public/javascripts/dashboard.js
@@ -8,4 +8,17 @@ $( document ).ready(function() {
         $(this).children(".dropdown").toggle();
     $(this).toggleClass('dropdownEn');
     });
+    var monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ];
+
+    var date = new Date();
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    console.log(day, monthNames[monthIndex], year);
 });
