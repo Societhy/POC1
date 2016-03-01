eeebac4cfa2e8bcf5c73f110adcd620192af1092
Titouan <titouan.theophile@gmail.com>
2016-03-01 18:24:11 +0100

    update user

--- server/public/javascripts/dashboard.js
+++ server/public/javascripts/dashboard.js
@@ -6,15 +6,6 @@ $( document ).ready(function() {
     var angle  = 180;
     $("li.togglable").on('click', function () {
         $(this).children(".dropdown").toggle();
-    if($(this).children(".dropdown").toggleClass('dropdownEn'))
-        if (angle == 270)
-            angle = 0;
-        else
-            angle = 270;
-
-        $(this).children("a").children("i").css({'-webkit-transform' : 'rotate('+angle+'deg)',
-            '-moz-transform' : 'rotate('+angle+'deg)',
-            '-ms-transform' : 'rotate('+angle+'deg)',
-            'transform' : 'rotate('+angle+'deg)'});
+    $(this).toggleClass('dropdownEn');
     });
 });
