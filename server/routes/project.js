9b4e66cc1be1c5f7ad2bd954c908eed54b450aba
Titouan <titouan.theophile@gmail.com>
2016-03-01 23:12:24 +0100

    ajout d'une modal pour les dons

--- server/routes/project.js
+++ server/routes/project.js
@@ -18,19 +18,23 @@ router.get('/:id', function (req, res, next)
             next(err);
             return;
         }
+        var i = 0;
         ret.object.fundraiseList.forEach(function(item) {
-            fundraise.getFundraise(ret.object.fundraiseList, function (fund) {
+                fundraise.getFundraise(item, function (fund) {
                 if (!fund.status) {
                     var err = new Error(fund.message);
                     err.status = 404;
                     next(err);
                     return;
+
                 }
                 ret.object.fundDetail = [];
+                fund.object.id = i;
                 ret.object.fundDetail.push(fund.object);
                 console.log(ret.object);
                 res.render('project_profile', {project: ret.object});
             });
+            i++;
         });
     });
 });
