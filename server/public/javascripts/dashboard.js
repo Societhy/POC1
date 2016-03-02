e10acda81e31ef9ab3642a1b94dc1d4128838cb1
Titouan <titouan.theophile@gmail.com>
2016-03-02 02:54:13 +0100

    fix problème d'affichage pour les membres/contributeurs

--- server/public/javascripts/dashboard.js
+++ server/public/javascripts/dashboard.js
@@ -8,17 +8,4 @@ $( document ).ready(function() {
         $(this).children(".dropdown").toggle();
     $(this).toggleClass('dropdownEn');
     });
-    var monthNames = [
-        "January", "February", "March",
-        "April", "May", "June", "July",
-        "August", "September", "October",
-        "November", "December"
-    ];
-
-    var date = new Date();
-    var day = date.getDate();
-    var monthIndex = date.getMonth();
-    var year = date.getFullYear();
-
-    console.log(day, monthNames[monthIndex], year);
 });
